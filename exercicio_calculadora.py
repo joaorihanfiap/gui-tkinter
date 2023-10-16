f18a92ea0d5a64643a2ba864f42725292cb28995
joaorihanfiap <rm99656@fiap.com.br>
2023-10-16 11:53:57 -0300

    paid

--- exercicio_calculadora.py
+++ exercicio_calculadora.py
@@ -15,17 +15,17 @@ titulo_pag.pack()
 container = Frame(window, padx=10, pady=15)
 container.pack()
 
-rotulo1 = Label(container, text="Nº 1", font="Arial 16", pady=5)
+rotulo1 = Label(container, text="Nº 1", font="Arial 16", pady=5, padx=5)
 rotulo1.pack(side=LEFT)
 
-caixa_texto1 = Entry(container, font=("Times New Roman", "16"), width=10)
+caixa_texto1 = Entry(container, font=("Times New Roman", "16"), width=5)
 caixa_texto1.pack(side=LEFT)
 
-caixa_texto2 = Entry(container, font=("Times New Roman", "16"), width=10)
+caixa_texto2 = Entry(container, font=("Times New Roman", "16"), width=5)
 caixa_texto2.pack(side=RIGHT)
 
 
-rotulo2 = Label(container, text="Nº 2", font="Arial 16", pady=5)
+rotulo2 = Label(container, text="Nº 2", font="Arial 16", pady=5, padx=5)
 rotulo2.pack(side=RIGHT)
 
 container2 = Frame(window, pady=10, padx=15)
@@ -34,18 +34,19 @@ container2.pack()
 container3 = Frame(window, pady=10, padx=15)
 container3.pack()
 
+
+
+botao = Button(container3, text="Somar", padx=10, pady=5, bg="purple", fg="white")
+botao["font"] = ("Courrier New", "16")
+botao["command"] = somar
+botao.pack()
+
 rotulo3 = Label(container2, font="Arial 16", padx=5, pady=10)
-rotulo3["text"] = "Soma: "
+rotulo3["text"] = "Resultado: "
 rotulo3.pack(side=LEFT)
 
 somatotal = Label(container2, font="Arial 16", padx=5, pady=10)
 somatotal.pack(side=LEFT)
-
-
-botao = Button(container3, text="Click Me!", padx=10, pady=5, bg="purple", fg="white")
-botao["font"] = ("Courrier New", "16")
-botao["command"] = somar
-botao.pack()
 
 caixa_texto1.focus()
 
